a023330f5ac39b361089d6a7daf8ee23c7a5cc75
jeff <jdagnogo11@gmail.com>
2025-02-24 10:15:16 +0200

    git init ios

---
+++ shared/src/iosMain/kotlin/com/carrefour/kmpcodemagic/MainViewController.kt
@@ -0,0 +1,10 @@
+package com.carrefour.kmpcodemagic
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+    }
+) {
+    App()
+}
